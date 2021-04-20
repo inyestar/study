81ff4b42488aa536a8d53d647e348a295023af97
inye <inye@DESKTOP-M6G42CL>
2021-04-20 14:13:53 +0900

    ApplicationContextAware, BeanNameAware 테스트

--- spring/src/main/java/com/inminhouse/study/spring/ch03/ByCustom.java
+++ spring/src/main/java/com/inminhouse/study/spring/ch03/ByCustom.java
@@ -1,6 +1,11 @@
 package com.inminhouse.study.spring.ch03;
 
-public class ByCustom {
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.BeanNameAware;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+
+public class ByCustom implements ApplicationContextAware, BeanNameAware{
 
 	public void init() {
 		System.out.printf("ByCustom.init() -------- initializing!!%n");
@@ -8,5 +13,15 @@ public class ByCustom {
 	
 	public void destroy() {
 		System.out.printf("ByCustom.destroy() -------- destroying!!%n");
+	}
+
+	@Override
+	public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+		System.out.printf("Gettting [%s] in %s%n", applicationContext.getClass().getSimpleName(), this.getClass().getName());
+	}
+
+	@Override
+	public void setBeanName(String name) {
+		System.out.printf("Gettting its name [%s] in %s%n", name, this.getClass().getName());
 	}
 }
